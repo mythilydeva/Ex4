ae4a57de4addedeb83587fe0147d844477ec593a
ITLAB <ITLAB@IT212>
2017-02-20 13:39:08 +0530

    commit2

--- src/com/example/sdcard/MainActivity.java
+++ src/com/example/sdcard/MainActivity.java
@@ -1,35 +1,64 @@
 package com.example.sdcard;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-
-public class MainActivity extends ActionBarActivity {
-
+import com.example.sdcard.FileOperations;
+ 
+public class MainActivity extends Activity {
+    EditText fname,fcontent,fnameread;
+    Button write,read;
+    TextView filecon;
+ 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        fname = (EditText)findViewById(R.id.fname);
+        fcontent = (EditText)findViewById(R.id.ftext);
+        fnameread = (EditText)findViewById(R.id.fnameread);
+        write = (Button)findViewById(R.id.btnwrite);
+        read = (Button)findViewById(R.id.btnread);
+        filecon = (TextView)findViewById(R.id.filecon);
+        write.setOnClickListener(new View.OnClickListener() {
+ 
+        @Override
+        public void onClick(View arg0) {
+            // TODO Auto-generated method stub
+        String filename = fname.getText().toString();
+        String filecontent = fcontent.getText().toString();
+        FileOperations fop = new FileOperations();
+        fop.write(filename, filecontent);
+        if(fop.write(filename, filecontent)){
+        Toast.makeText(getApplicationContext(), filename+".txt created", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(getApplicationContext(), "I/O error", Toast.LENGTH_SHORT).show();
+ 
         }
-        return super.onOptionsItemSelected(item);
+        }
+    });
+    read.setOnClickListener(new View.OnClickListener() {
+ 
+        @Override
+        public void onClick(View arg0) {
+            // TODO Auto-generated method stub
+            String readfilename = fnameread.getText().toString();
+            FileOperations fop = new FileOperations();
+            String text = fop.read(readfilename);
+            if(text != null){
+            filecon.setText(text);
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "File not Found", Toast.LENGTH_SHORT).show();
+                filecon.setText(null);
+            }
+ 
+        }
+    });
     }
-}
+    }
